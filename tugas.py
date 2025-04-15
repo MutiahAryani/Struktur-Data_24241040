812caa8b93681a7bf7d620058f83dcb9954d02e8
MutiahAryani <mutiaaryani86@gmail.com>
2025-04-14 22:54:03 -0400

    Add files via upload

---
+++ tugas.py
@@ -0,0 +1,8 @@
+rows = int(input(""))
+
+for j in range(rows):
+    for j in range(rows - i - 1):
+        print(" ", end="")
+    for j in range(rows - i - 1):
+        print("*", end="")
+    print()

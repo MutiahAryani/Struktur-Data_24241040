b89599ed9ceb7606097395b68ea314b60f2f3a2e
MutiahAryani <mutiaaryani86@gmail.com>
2025-04-15 00:56:33 -0400

    Delete PRE-PRATIKUM directory

--- PRE-PRATIKUM/tugas3.py
+++
@@ -1,9 +0,0 @@
-def fibonacci_banyak_elemen(n):
-    fib = [1, 2]
-    while len(fib) < n:
-        fib.append(fib[-1] + fib[-2])
-    print("Output:", ",".join(map(str, fib[:n])))
-
-# Contoh penggunaan
-jumlah = int(input("Masukkan jumlah deret: "))
-fibonacci_banyak_elemen(jumlah)
